5ee4946be56ee70b9e12e8887a52e91475051213
hao-popos <haovoanh28@gmail.com>
2024-11-25 21:51:58 +0700

    update

--- internal/models/city.go
+++ internal/models/city.go
@@ -1,10 +1,13 @@
 package models
 
 type City struct {
-	ID        uint       `gorm:"primaryKey"`
-	Name      string     `gorm:"column:name;index"`
-	Code      string     `gorm:"column:code;index"`
-	Districts []District `gorm:"foreignKey:CityID;references:ID;constraint:OnDelete:CASCADE"`
+	ID          uint       `gorm:"primaryKey"`
+	DisplayName string     `gorm:"column:display_name"`
+	Name        string     `gorm:"column:name;index"`
+	ShortName   string     `gorm:"column:short_name;index"`
+	EngName     string     `gorm:"column:eng_name;index"`
+	Code        string     `gorm:"column:code;index"`
+	Districts   []District `gorm:"foreignKey:CityID;references:ID;constraint:OnDelete:CASCADE"`
 }
 
 func (City) TableName() string {
--- internal/models/district.go
+++ internal/models/district.go
@@ -1,11 +1,13 @@
 package models
 
 type District struct {
-	ID        uint   `gorm:"primaryKey"`
-	Name      string `gorm:"column:name;index"`
-	ShortName string `gorm:"column:short_name;index"`
-	Code      string `gorm:"column:code;index"`
-	CityID    uint   `gorm:"column:city_id;index"`
+	ID          uint   `gorm:"primaryKey"`
+	DisplayName string `gorm:"column:display_name"`
+	Name        string `gorm:"column:name;index"`
+	ShortName   string `gorm:"column:short_name;index"`
+	EngName     string `gorm:"column:eng_name;index"`
+	Code        string `gorm:"column:code;index"`
+	CityID      uint   `gorm:"column:city_id;index"`
 
 	City    City     `gorm:"foreignKey:CityID;"`
 	Streets []Street `gorm:"foreignKey:DistrictID;references:ID;constraint:OnDelete:CASCADE"`
