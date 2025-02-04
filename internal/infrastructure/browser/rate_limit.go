06c48842f8ca37413906e8ea48340acee0a5a2af
hao-popos <haovoanh28@gmail.com>
2025-02-04 22:52:31 +0700

    fix: work time sometimes isn't right

--- internal/infrastructure/browser/rate_limit.go
+++ internal/infrastructure/browser/rate_limit.go
@@ -7,31 +7,32 @@ import (
 
 type RateLimiter struct {
 	interval time.Duration
-	ticker   *time.Ticker
+	//ticker   *time.Ticker
+	timer *time.Timer
 }
 
 func NewBrowserRateLimiter(interval time.Duration) *RateLimiter {
-	// Get a random duration between -5s and +5s
-	// rand.Int63n(11) - 5  // 0-10 minus 5 gives us -5 to +5
-	jitter := time.Duration(rand.Int63n(11)-5) * time.Second
-
-	// Make sure we don't go negative
-	finalInterval := interval
-	if interval > 5*time.Second {
-		finalInterval = interval + jitter
-	} else {
-		// For small intervals, only add positive jitter
-		finalInterval = interval + time.Duration(rand.Int63n(5))*time.Second
-	}
-
 	return &RateLimiter{
-		interval: finalInterval,
-		ticker:   time.NewTicker(interval),
+		interval: interval,
+		//ticker:   time.NewTicker(interval),
+		timer: time.NewTimer(interval),
 	}
 }
 
 func (r *RateLimiter) Wait() {
-	<-r.ticker.C
+	<-r.timer.C
+
+	// Calculate new random interval
+	jitter := time.Duration(rand.Int63n(11)-5) * time.Second
+	nextInterval := r.interval
+	if r.interval > 5*time.Second {
+		nextInterval = r.interval + jitter
+	} else {
+		nextInterval = r.interval + time.Duration(rand.Int63n(6))*time.Second
+	}
+
+	// Reset timer with new random interval
+	r.timer.Reset(nextInterval)
 }
 
 func (r *RateLimiter) UpdateInterval(interval time.Duration) {
